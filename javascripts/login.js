5975e2cba1323327e1f8c5b48d81bd728960bace
MarufHasanBhuiyan <bhuiyan22205101134@diu.edu.bd>
2025-10-18 00:52:00 +0600

    final homepage and css

---
+++ javascripts/login.js
@@ -0,0 +1,10 @@
+function togglerAdmin(){
+    document.getElementById("admin").style.display = "block";
+    document.getElementById("user").style.display = "none";
+   
+   
+}
+function togglerUser(){
+    document.getElementById("user").style.display = "block";
+    document.getElementById("admin").style.display = "none";
+}
